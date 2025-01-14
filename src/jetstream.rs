62ee2f6784c6399d455e071fcba4fd3fb2e0a592
danielhe4rt <danielhe4rt@gmail.com>
2025-01-14 18:56:51 -0300

    chore: split jetstream concerns

---
+++ src/jetstream.rs
@@ -0,0 +1,39 @@
+use crate::events::events_handler;
+use crate::repositories::DatabaseRepository;
+use atrium_api::types::string::Nsid;
+use jetstream_oxide::events::JetstreamEvent::Commit;
+use jetstream_oxide::{
+    DefaultJetstreamEndpoints, JetstreamCompression, JetstreamConfig, JetstreamConnector,
+};
+use paris::info;
+use std::sync::Arc;
+
+pub async fn start_jetstream(repository: &Arc<DatabaseRepository>) {
+    let config = JetstreamConfig {
+        endpoint: DefaultJetstreamEndpoints::USEastTwo.into(),
+        wanted_collections: vec![
+            Nsid::new("app.bsky.feed.post".to_string()).expect("Failed to create NSID"),
+            Nsid::new("app.bsky.feed.like".to_string()).expect("Failed to create NSID"),
+            Nsid::new("app.bsky.feed.repost".to_string()).expect("Failed to create NSID"),
+        ],
+        wanted_dids: vec![
+            //Did::new("did:plc:doqrpcaai4iqmkbdo3ztmlld".to_string()).expect("Failed to create DID"),
+        ],
+        compression: JetstreamCompression::Zstd,
+        cursor: None,
+    };
+
+    let (receiver, _) = JetstreamConnector::new(config)
+        .expect("Failed to create Jetstream connector")
+        .connect()
+        .await
+        .expect("Failed to connect to Jetstream");
+
+    info!("Starting Jetstream listener");
+
+    while let Ok(event) = receiver.recv_async().await {
+        if let Commit(commit) = event {
+            events_handler(repository, commit).await;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,70 @@
 //! A very basic example of how to listen for create/delete events on a specific DID and NSID.
 
 mod events;
+mod http;
+mod jetstream;
 mod leveling;
-
-mod crawler;
 mod models;
 mod repositories;
 
-use crate::events::events_handler;
-use atrium_api::types::string::Nsid;
-use jetstream_oxide::{
-    events::JetstreamEvent::Commit, DefaultJetstreamEndpoints, JetstreamCompression,
-    JetstreamConfig, JetstreamConnector,
-};
-use paris::{info, Logger};
+use paris::Logger;
 use scylla::{CachingSession, SessionBuilder};
 
+use crate::http::start_http;
+use crate::jetstream::start_jetstream;
+use actix_web::rt::signal;
+use scylla::transport::session::{CurrentDeserializationApi, GenericSession};
 use std::sync::Arc;
+use tokio::task::JoinSet;
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() {
     Logger::new();
+    env_logger::init();
 
-    let config = JetstreamConfig {
-        endpoint: DefaultJetstreamEndpoints::USEastTwo.into(),
-        wanted_collections: vec![
-            Nsid::new("app.bsky.feed.post".to_string()).expect("Failed to create NSID"),
-            Nsid::new("app.bsky.feed.like".to_string()).expect("Failed to create NSID"),
-            Nsid::new("app.bsky.feed.repost".to_string()).expect("Failed to create NSID"),
-        ],
-        wanted_dids: vec![],
-        compression: JetstreamCompression::Zstd,
-        cursor: None,
-    };
+    let session = start_scylla_session().await;
+    let caching_session = Arc::new(CachingSession::from(session, 50));
 
+    let repository = Arc::new(repositories::DatabaseRepository::new(Arc::clone(
+        &caching_session,
+    )));
+
+    let mut join = JoinSet::new();
+    let jetstream_repository = Arc::clone(&repository);
+    join.spawn(async move {
+        start_jetstream(&jetstream_repository).await;
+    });
+
+    join.spawn(async move {
+        let _ = start_http(&repository).await;
+    });
+
+    // Listen for Ctrl+C (SIGINT) or termination signals (SIGTERM)
+    let signal_handle = tokio::spawn(async {
+        signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
+        println!("Ctrl+C pressed. Shutting down...");
+    });
+
+    // Wait for tasks or signals
+    tokio::select! {
+        _ = signal_handle => {
+            println!("Received shutdown signal.");
+        }
+        _ = async {
+            while let Some(task) = join.join_next().await {
+                if let Err(e) = task {
+                    eprintln!("Task failed: {:?}", e);
+                }
+            }
+        } => {
+            println!("All tasks completed or terminated.");
+        }
+    }
+
+    println!("Connection to Jetstream lost. Application shutting down.");
+}
+
+async fn start_scylla_session() -> GenericSession<CurrentDeserializationApi> {
     let session = SessionBuilder::new()
         .known_nodes(vec![
             "localhost:19042",
@@ -48,26 +79,5 @@ async fn main() {
         .use_keyspace("bsky_rpg", false)
         .await
         .expect("Failed to use keyspace");
-
-    let caching_session = Arc::new(CachingSession::from(session, 50));
-
-    let (receiver, _) = JetstreamConnector::new(config)
-        .expect("Failed to create Jetstream connector")
-        .connect()
-        .await
-        .expect("Failed to connect to Jetstream");
-
-    info!("Starting Jetstream listener");
-
-    let repository = Arc::new(repositories::DatabaseRepository::new(Arc::clone(
-        &caching_session,
-    )));
-
-    while let Ok(event) = receiver.recv_async().await {
-        if let Commit(commit) = event {
-            events_handler(&repository, commit).await;
-        }
-    }
-
-    println!("Connection to Jetstream lost.");
+    session
 }
