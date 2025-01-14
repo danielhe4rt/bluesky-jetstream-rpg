4c1f8e4c07548bb9757d490cb93b90aeeb04687b
danielhe4rt <danielhe4rt@gmail.com>
2025-01-14 18:56:07 -0300

    feat: add bsky client repository

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -1,6 +1,8 @@
+mod bsky_repository;
 pub mod character_repository;
 pub mod event_repository;
 
+use crate::repositories::bsky_repository::BskyRepository;
 use crate::repositories::character_repository::CharacterRepository;
 use crate::repositories::event_repository::EventRepository;
 use scylla::CachingSession;
@@ -9,6 +11,7 @@ use std::sync::Arc;
 pub struct DatabaseRepository {
     pub character: CharacterRepository,
     pub event: EventRepository,
+    pub bsky: BskyRepository,
 }
 
 impl DatabaseRepository {
@@ -16,6 +19,7 @@ impl DatabaseRepository {
         Self {
             character: CharacterRepository::new(Arc::clone(&connection)),
             event: EventRepository::new(Arc::clone(&connection)),
+            bsky: BskyRepository::new("https://public.api.bsky.app".to_string()),
         }
     }
 }
